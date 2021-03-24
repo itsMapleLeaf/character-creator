affc104060fae26b79416a52b5426ef51cab8775
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-03-23 23:45:12 -0500

    add a shade rectangle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react"
-
+import styles from "./App.module.css"
 import CharacterEditor from "./components/CharacterEditor"
 import Footer from "./components/Footer"
 
 function App() {
   return (
     <>
+      <div className={styles.shade} />
       <CharacterEditor />
       <Footer />
     </>
